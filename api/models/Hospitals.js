6f62d09e8b8b3495dafd8f729132951462fbcca4
fasilajibriel <fasilajibriel@users.noreply.github.com>
2021-06-03 12:14:29 +0300

    Heroku Initial

---
+++ api/models/Hospitals.js
@@ -0,0 +1,59 @@
+/**
+ * Hospitals.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  schema: true,
+  tableName: 'hospitals',
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+    id: {
+      type: 'number',
+      autoIncrement: true,
+      columnName: 'id',
+      required: true
+    },
+    name: {
+      type: 'string',
+      columnName: 'hospital_name',
+      required: true
+    },
+    address: {
+      type: 'string',
+      columnName: 'hospital_address',
+      required: true
+    },
+    coordinates: {
+      type: 'string',
+      columnName: 'hospital_coordinates',
+      required: true
+    },
+    phone: {
+      type: 'string',
+      columnName: 'hospital_phonenumber',
+      required: true
+    },
+
+  },
+
+};
+
